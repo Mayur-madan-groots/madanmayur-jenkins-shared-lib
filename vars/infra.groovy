459c1e8eb87ef71f7746e110ada3189499f9d80d
Ubuntu <mayur>
2022-12-21 11:03:28 +0530

    for test

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -1,5 +1,6 @@
+def call() {
     def scriptcontents = libraryResource "com/planetpope/scripts/linux/infra.sh"
     writeFile file: "infra.sh", text: scriptcontents 
     sh "chmod a+x ./infra.sh"
-
+}
 
